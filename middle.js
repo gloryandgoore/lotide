5de96c59e971de224961dcd4d3c6d0130fe3fbde
Caseyann <goore_44@hotmail.ca>
2021-11-29 18:04:39 -0500

    refactored

--- middle.js
+++ middle.js
@@ -1,23 +1,6 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  }
-  return true;
-    
-};
-  
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`They are the same.`);
-  } else {
-    console.log(`They are different.`);
-  }
-};
+const eqArrays = require('./eqArrays')
+const assertArraysEqual= require('./assertArraysEqual')
+
     
 const middle = function(array) {
 
@@ -44,9 +27,6 @@ const middle = function(array) {
 //console.log(middle, [1, 2, 3, 4, 5, 6]) // => [3, 4])
 
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6]));// => [3, 4]
+
+
+module.exports = middle;
