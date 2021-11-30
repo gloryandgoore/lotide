c633c41e0745ecfd594c75ed1851ec409fb78d4b
Caseyann <goore_44@hotmail.ca>
2021-11-30 00:12:35 -0500

    refactored

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const head   = require('./head');
-const tail   = require('./tail');
+const head = require('./head');
+const tail = require('./tail');
 const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual')
 const assertObjectsEqual   = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
 const countOnly = require('./countOnly');
-const eqArrays   = require('./eqArrays');
+const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 const findKey = require('./findKey');
-const findKeyByValue   = require('./findKeyByValue');
+const findKeyByValue = require('./findKeyByValue');
 const letterPositions = require('./letterPositions');
 const map = require('./map');
 const takeUntil  = require('./takeUntil');
 const without  = require('./without');
 
-module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
-
-
-};
+let someIndex = {
+    head,
+    tail,
+    middle,
+    assertArraysEqual,
+    assertEqual,
+    assertObjectsEqual,
+    countLetters,
+    countOnly,
+    eqArrays,
+    eqObjects,
+    findKey,
+    findKeyByValue,
+    letterPositions,
+    map,
+    takeUntil,
+    without
+}
+//console.log(someIndex);
+module.exports = someIndex;
