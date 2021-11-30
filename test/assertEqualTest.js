9f7d6021b040806cb8b3aff293672217c40f4c32
Caseyann <goore_44@hotmail.ca>
2021-11-29 21:38:20 -0500

    mocha test

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,4 +1,15 @@
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert
+describe("#assertEqual", () => {
+    it ("returns '✅✅✅  Assertion Passed: 1 === 1' for 1, 1", () => {
+        assert.deepEqual(assertEqual(1, 1));
+    });
+    it("returns '🛑🛑🛑 Assertion Failed: Lighthouse Labs !== Bootcamp' for 'Lighthouse Labs', 'Bootcamp'", () => {
+        assert.deepEqual(assertEqual("Lighthouse Labs", "Bootcamp"));
+    });
+    
+});
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
