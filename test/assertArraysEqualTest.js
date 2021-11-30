938a54ec37bd22d9605ffa53401559968a0a83b7
Caseyann <goore_44@hotmail.ca>
2021-11-29 21:44:31 -0500

    mocha test

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,3 +1,11 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+describe("#assertArrayEqual", () => {
+    it ("returns They are the same. for [1, 2, 3], [1, 2, 3]", () => {
+        assert.deepEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+    });
+    
+});
+
+//assertArraysEqual([1, 2, 3], [1, 2, 3]);
